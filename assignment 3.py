5ed0a6044beb0aa4c10f9c3baf8355c775d9b86a
shahryar <sheikhsheri135@gmail.com>
2025-09-01 04:10:45 -0700

    3 assignment

---
+++ assignment 3.py
@@ -0,0 +1,62 @@
+Python 3.12.10 (tags/v3.12.10:0cc8128, Apr  8 2025, 12:21:36) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+x  =  {1,2,3}
+id x
+SyntaxError: invalid syntax
+id (x)
+2179855441184
+
+======================== RESTART: C:/Users/user/Documents/python 3.py =======================
+Traceback (most recent call last):
+  File "C:/Users/user/Documents/python 3.py", line 8, in <module>
+    for score in scores:
+NameError: name 'scores' is not defined. Did you mean: 'scoress'?
+
+======================== RESTART: C:/Users/user/Documents/python 3.py =======================
+Traceback (most recent call last):
+  File "C:/Users/user/Documents/python 3.py", line 8, in <module>
+    for score in Scores:
+NameError: name 'Scores' is not defined. Did you mean: 'scoress'?
+>>> 
+======================== RESTART: C:/Users/user/Documents/python 3.py =======================
+Traceback (most recent call last):
+  File "C:/Users/user/Documents/python 3.py", line 10, in <module>
+    a.append(score)
+NameError: name 'a' is not defined
+>>> 
+======================== RESTART: C:/Users/user/Documents/python 3.py =======================
+Traceback (most recent call last):
+  File "C:/Users/user/Documents/python 3.py", line 10, in <module>
+    a.append(score)
+NameError: name 'a' is not defined
+>>> 
+======================== RESTART: C:/Users/user/Documents/python 3.py =======================
+Welcome to SMIT training center, Ahsan
+Negative
+25
+25
+Adult
+Odd
+36
+Pass
+120
+Ahsan
+20
+AI and Data Science
+True
+(153.93804002589985, 43.982297150257104)
+Strings: ['Apple', 'Data Science']
+Numbers: [44, 74, True, False]
+Booleans: []
+True
+['apple', 'banana', 'car', 'bike', 'aeroplane']
+First Best: 400
+Second Best: 95
+Ascending: [32, 54, 71]
+Descending: [71, 54, 32]
+Average: 52.333333333333336
+Strings Asc: ['Camera', 'Laptop', 'Marker', 'Speakers']
+Strings Desc: ['Speakers', 'Marker', 'Laptop', 'Camera']
+Numbers Asc: [3, 10.0, 10, 310.25]
+Numbers Desc: [310.25, 10.0, 10, 3]
+['apple', 'orange', 'apricot', 'orange', 'cherry']
